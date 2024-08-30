49315a3894970d8af993c90835c018b9419014d6
Mckee Frazior <info@mckeefrazior.com>
2024-08-30 14:45:19 -0500

    some font tweaking

--- pages/index.js
+++ pages/index.js
@@ -28,11 +28,12 @@ export default function Home() {
               <Link  href='/lajolla-audio'>New Viewpoints @ La Jolla 1983</Link>
               <Link  href='/lajolla-outside-audio'>Outside Views @ La Jolla 1983</Link>
             </div>
-            <p className="description">
-            Do you really believe that? is conceived in honor and celebration of Dr. Frances Colpitt, her renowned scholarship, critical pedagogy and unyielding commitment to teaching along with her profound respect for artists, their work, and critics alike. The exhibition is guided by themes coalesced from Colpitt’s work over the course of her career with a focus on abstraction, Minimalism and Conceptual art. They illuminate her scholarship and curatorial practice, and also emphasize the significant impact and enduring legacy of her mentorship of artists in Texas. 
-            </p>
+          
           </div>
         </div>
+        <p className="description">
+            Do you really believe that? is conceived in honor and celebration of Dr. Frances Colpitt, her renowned scholarship, critical pedagogy and unyielding commitment to teaching along with her profound respect for artists, their work, and critics alike. The exhibition is guided by themes coalesced from Colpitt’s work over the course of her career with a focus on abstraction, Minimalism and Conceptual art. They illuminate her scholarship and curatorial practice, and also emphasize the significant impact and enduring legacy of her mentorship of artists in Texas. 
+            </p>
       </main>
 
       <Footer />
